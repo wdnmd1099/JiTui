3cda1a52d04717297688856ced7a6e36ef713382
Kasan <879611700@qq.com>
2023-01-04 20:24:04 +0800

    更新tabs组件，支持强制重新渲染或者一次全部渲染

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -4,10 +4,14 @@ export const Tabs = defineComponent({
     props: {
         selected: {
             type: String as PropType<string>,
-            required:false
+            required: false,
+        },
+        rerenderOnSelected: {
+            type: Boolean as PropType<boolean>,
+            default: false,
         },
     },
-    emits:['update:selected'],
+    emits: ['update:selected'],
     setup(props, context) {
         return () => {
             const tabsArray = context.slots?.default?.()
@@ -23,18 +27,25 @@ export const Tabs = defineComponent({
                 <ol class={s.tabs_nav}>
                     {tabsArray.map(item =>
                         <li class={item.props?.name === props.selected ? s.selected : ''}
-                        onClick={()=> {context.emit('update:selected',item.props?.name)}
-                    }
+                            onClick={() => { context.emit('update:selected', item.props?.name) }
+                            }
                         // 被点击的li的name传给selected，双向绑定，所以name 和 selected 永远相等
                         // 然后给被点击的li 加上样式
                         >
-                         {item.props?.name}
+                            {item.props?.name}
                         </li>)}
                 </ol>
                 <div>
-                    {tabsArray.map( item => 
-                        (<div v-show = {item.props?.name === props.selected}>{item}</div>)
-                    )}
+                    {/*false是不需要它重新渲染，只需要全部渲染一次就可以了
+                    ，charts插件一开始不能获取display：none的宽高，不能渲染，所以加了个rerenderOnSelected选项，true就强制重新渲染*/}
+                    {props.rerenderOnSelected === false ? 
+                        tabsArray.map(item =>   
+                            (<div v-show={item.props?.name === props.selected}>{item}</div>) 
+                        ) :
+                        <div key={props.selected}> {/*key={props.selected} 不会被浏览器缓存导致动画缺失*/}
+                            {tabsArray.find(item => item.props?.name === props.selected)}
+                        </div>
+                    }
                     {/* 一开始写成下面这句，导致初始化的时候只跑了一次，不能使所有的tab组件都跑一次，导致bug */}
                     {/* {tabsArray.find(item => item.props?.name === props.selected)} */}
                     {/* 找到当前选中的Tab组件，把它的context.slots?.default渲染到页面,不理解的话打开下面的关闭上面的就知道了 */}
