b53411d37998b2a35fedb583a6563ef98c75447f
Kasan <879611700@qq.com>
2023-03-22 16:49:20 +0800

    更改欢迎页背景色

--- src/vite_plugins/svgstore.js
+++ src/vite_plugins/svgstore.js
@@ -1,20 +1,21 @@
 /* eslint-disable */
+//这是一个vite的插件，它的作用是制作svg Sprites 图
 import path from 'path'
 import fs from 'fs'
 import store from 'svgstore' // 用于制作 SVG Sprites
 import { optimize } from 'svgo' // 用于优化 SVG 文件
 
-export const svgstore = (options = {}) => {
+export const svgstore = (options = {}) => { // 在main.ts 引入了@svgstore，但是main.ts找不到，所以需要在这解析它
   const inputFolder = options.inputFolder || 'src/assets/icons';
   return {
     name: 'svgstore',
     resolveId(id) {
       if (id === '@svgstore') {
-        return 'svg_bundle.js'
+        return 'svg_bundle.js' //如果发现是@svgstore，就return 一个svg_bundle.js，为什么不直接引入svg_bundle.js，因为浏览器支持，但vscode可能不支持，此举纯属为了兼容一些比较弱智的编辑器
       }
     },
     load(id) {
-      if (id === 'svg_bundle.js') {
+      if (id === 'svg_bundle.js') { //下面就是把小的svg 放到一个大的svg，然后使用 svgo 优化这些svg的代码，比如删掉一些没用的空格之类的
         const sprites = store(options);
         const iconsDir = path.resolve(inputFolder);
         for (const file of fs.readdirSync(iconsDir)) {
